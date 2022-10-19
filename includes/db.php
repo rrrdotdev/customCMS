3f959cbef6ec847590f0077f112123a9744fc156
RRR <renaydev@gmail.com>
2022-10-19 15:58:36 -0500

    updates on index.php and includes with sub files

--- includes/db.php
+++ includes/db.php
@@ -5,13 +5,17 @@ $db['db_user'] = "root";
 $db['db_pass'] = "root";
 $db['db_name'] = "cms";
 
+
 foreach ($db as $key => $value){
 
-
+//uppercase all keys for CONSTANT
+define(strtoupper($key), $value);
 }
+//connect to database
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
-$connection = mysqli_connect('localhost', 'root','root','cms');
-
+//confirm database connection
 if ($connection) {
     echo "We are connected";
 }
+
